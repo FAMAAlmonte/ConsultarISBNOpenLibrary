3a3c1315e982f4074d83912dd258d78d0ecff108
Mauro A Flores A <mauroa_99@hotmail.com>
2016-08-14 02:13:57 -0500

    Initial Commit

---
+++ ConsultarISBNOpenLibrary/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ConsultarISBNOpenLibrary
+//
+//  Created by Mauro Alberto Flores Almonte on 14/08/16.
+//  Copyright © 2016 Mauro Alberto Flores Almonte. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
