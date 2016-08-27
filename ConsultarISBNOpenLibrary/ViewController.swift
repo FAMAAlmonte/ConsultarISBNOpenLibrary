c105d175d451972cd0ee4bead84ce16b93d9cb7b
Mauro A Flores A <mauroa_99@hotmail.com>
2016-08-27 02:00:11 -0500

    Para revisión

--- ConsultarISBNOpenLibrary/ViewController.swift
+++ ConsultarISBNOpenLibrary/ViewController.swift
@@ -8,16 +8,61 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITextFieldDelegate {
+    
 
-    override func viewDidLoad() {
+
+    @IBOutlet weak var errorEtiqueta: UITextView!
+
+    
+    @IBOutlet weak var mostrarTexto: UITextView!
+    @IBOutlet weak var ISBNTextField: UITextField!
+    
+    
+    /** captura el ISBN */
+    @IBAction func capturaISBN(sender: AnyObject) {
+        
+        if ISBNTextField.text?.containsString(" ") == true {  //Evita el error de nil o caracteres distintos a numeros
+            ISBNTextField.text? = ""
+        } else {
+            // Busqueda de la url
+        var url = NSURL(string: "https://openlibrary.org/api/books?jscmd=data&format=json&bibkeys=ISBN:" + ISBNTextField.text!)
+        var datos:NSData = NSData(contentsOfURL: url!)!
+        var texto = NSString(data: datos, encoding: NSUTF8StringEncoding)
+        
+        
+        
+        if texto!.containsString("{}"){
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                // Error 
+                self.errorEtiqueta.text = "¡ISBN, de libro No encontrado!,  ¡Intenta de nuevo, por favor!.  No debe contener espacios, guiones ó simbolos "
+                
+            }) } else {
+            
+            //  imprime la busqueda
+            dispatch_async(dispatch_get_main_queue(), { () -> Void in
+                
+                print(texto)
+                self.mostrarTexto.text! = texto as! String
+                
+            })
+            }
+        }
+        
+    func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
+    
+        
     }
 
-    override func didReceiveMemoryWarning() {
+    func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+        }
+        
     }
 
 
